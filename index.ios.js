5b6e88c26742aa81bb0cc20338491333832c8c3d
Forrest VanderBorgh <forrest.vanderborgh@flightly.com>
2017-06-10 19:12:51 -0700

    This kind of works

--- index.ios.js
+++ index.ios.js
@@ -20,93 +20,80 @@ export default class forceTouchTest extends Component {
   constructor(props) {
     super(props);  
     this._touchable = this._touchable.bind(this);
-    this._panResponderChild = this._panResponderChild.bind(this);
-    this._panResponderParent = this._panResponderParent.bind(this);
     this.state = {
       panViewIsTapped: false
     }
   }
 
   componentWillMount() {
-    this._panResponderChild();
-    this._panResponderParent();
-  }
+    this._panResponderChild = PanResponder.create({
 
-  _panResponderParent() {
-    return(
-      PanResponder.create({
+      // true because we want tapping on the cal, to set it as a responder
+      onStartShouldSetPanResponder: () => true,
 
-        // true because we want tapping on the cal, to set it as a responder
-        onStartShouldSetPanResponder: () => true,
+      // false because we DON'T want to grab the responder lock from our children on start
+      onStartShouldSetPanResponderCapture: () => false,
 
-        // false because we DON'T want to grab the responder lock from our children on start
-        onStartShouldSetPanResponderCapture: () => false,
+      /*
+      onMoveShouldSetPanResponderCapture:
+        false because we don't want to accidentally grab the responder lock from
+        our children on movement.
+          That's because sometimes even a small tap contains movement,
+          and thus a big percentage of taps will not work.
+          Keeping that flag false does not nessecarily mean that our children will
+         always capture the responder lock on movement, (they can if they want),
+         we're just not strict enough to grab it from them.
+      */
+      onMoveShouldSetPanResponderCapture: () => false,
 
-        /*
-        onMoveShouldSetPanResponderCapture:
-          false because we don't want to accidentally grab the responder lock from
-          our children on movement.
-            That's because sometimes even a small tap contains movement,
-            and thus a big percentage of taps will not work.
-            Keeping that flag false does not nessecarily mean that our children will
-           always capture the responder lock on movement, (they can if they want),
-           we're just not strict enough to grab it from them.
-        */
-        onMoveShouldSetPanResponderCapture: () => false,
+      /*
+      onMoveShouldSetPanResponder:
+        We DO want moving the finter on the cal, to set it as a responder,
+        BUT, we don't always want moving the finger on an appointment setting this parent
+        as the responder.
+ 
+        Logic:
+        So, when the dx AND dy of the pan are 0, we return false, because we don't
+        want to grab the responder from our appointment children.
+ 
+        For anything other than that we just allow this parent to become the responder.
+ 
+        (dx, dy: accumulated distance of the gesture since the touch started)
+      */
+      onMoveShouldSetPanResponder: (e, gestureState) =>
+        !(gestureState.dx === 4 || gestureState.dy === 4),
+    }),
 
-        /*
-        onMoveShouldSetPanResponder:
-          We DO want moving the finter on the cal, to set it as a responder,
-          BUT, we don't always want moving the finger on an appointment setting this parent
-          as the responder.
-  
-          Logic:
-          So, when the dx AND dy of the pan are 0, we return false, because we don't
-          want to grab the responder from our appointment children.
-  
-          For anything other than that we just allow this parent to become the responder.
-  
-          (dx, dy: accumulated distance of the gesture since the touch started)
-        */
-        onMoveShouldSetPanResponder: (e, gestureState) =>
-          !(gestureState.dx === 0 || gestureState.dy === 0),
-      })
-    );
-  }
+    this._panResponderParent = PanResponder.create({
+      // true because we want tapping on this to set it as the responder
+      onStartShouldSetPanResponder: () => true,
 
-  _panResponderChild() {
-    return (
-      PanResponder.create({
-        // true because we want tapping on this to set it as the responder
-        onStartShouldSetPanResponder: () => true,
+      // true because we want this to capture the responder lock from it's parent on start
+      onStartShouldSetPanResponderCapture: () => true,
 
-        // true because we want this to capture the responder lock from it's parent on start
-        onStartShouldSetPanResponderCapture: () => true,
+      // when the pan responder lock is terminated, set the pan view as NOT tapped
+      onPanResponderTerminate: () => {
+        this.setState({ panViewIsTapped: false });
+      },
 
-        // when the pan responder lock is terminated, set the pan view as NOT tapped
-        onPanResponderTerminate: () => {
-          this.setState({ panViewIsTapped: false });
-        },
+      // true so that the parent can grab our responder lock if he wan'ts to.
+      onPanResponderTerminationRequest: () => true,
 
-        // true so that the parent can grab our responder lock if he wan'ts to.
-        onPanResponderTerminationRequest: () => true,
+      // false because we DON'T want this btn capturing the resp lock from it's parent on move
+      onMoveShouldSetPanResponderCapture: () => false,
 
-        // false because we DON'T want this btn capturing the resp lock from it's parent on move
-        onMoveShouldSetPanResponderCapture: () => false,
+      // false because we DON'T want moving the finger on this to set it as the responder
+      onMoveShouldSetPanResponder: () => false,
 
-        // false because we DON'T want moving the finger on this to set it as the responder
-        onMoveShouldSetPanResponder: () => false,
+      onPanResponderGrant: () => {
+        this.setState({ panViewIsTapped: true });
+      },
 
-        onPanResponderGrant: () => {
-          this.setState({ panViewIsTapped: true });
-        },
-
-        onPanResponderRelease: () => {
-          this.setState({ panViewIsTapped: false });
-          console.log('hello world');
-        },
-      })
-    );
+      onPanResponderRelease: () => {
+        this.setState({ panViewIsTapped: false });
+        console.log('hello world');
+      },
+    })
   }
 
   _touchable() {
@@ -161,6 +148,53 @@ export default class forceTouchTest extends Component {
   }
 
   render() {
+    let touchable;
+    const touchableText = 'This should be force touchable!';
+    const touchableChildren = (
+      <Text>
+        {touchableText}
+      </Text>
+    );
+
+    if (View.forceTouchAvailable === false) {
+      touchable = (
+        <TouchableHighlight
+          onPress={() => { console.log('forceTouchAvailable = false'); }}
+          style={styles.noForceTouch}
+          underlayColor={styles.underlayHighlight}
+        >
+          {touchableChildren}
+        </TouchableHighlight>
+      );
+    } else {
+      touchable = (
+        <View
+          style={[styles.yesForceTouch, {
+            backgroundColor: this.state.panViewIsTapped ?
+              styles.forceTouchHighlightBgColorTapped
+              :
+              styles.forceTouchHighlightBgColorNotTapped,
+          }]}
+          {...this._panResponderChild.PanHandlers} //_panResponderChild
+        >
+          {touchableChildren}
+        </View>
+      )
+    }
+    return (
+      <View style={styles.otherContainer}>
+      <View {...this._panResponderParent.panHandlers}>
+        <Animated.View>
+          <View>
+            {touchable}
+          </View>
+        </Animated.View>
+      </View>
+      </View>
+    )
+  }
+/*
+  render() {
     const renderTouchable = this._touchable();
 
     return (
@@ -169,6 +203,7 @@ export default class forceTouchTest extends Component {
       </View>
     );
   }
+  */
 }
 
 const styles = StyleSheet.create({
